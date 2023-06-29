e04300371efb1c360a3fc1fa5e85d4a161902be3
Gustav <g.alerby@gmail.com>
2023-06-29 16:45:58 +0200

    Added bring your own salt to the frontend

--- static/script/script.js
+++ static/script/script.js
@@ -2,20 +2,31 @@ document.addEventListener("DOMContentLoaded", function () {
     var submitButton = document.getElementById("submit_button");
     var copyButton = document.getElementById("copy_button");
     var messageInput = document.getElementById("message_input");
+    var saltInput = document.getElementById("salt_input");
     var linkElement = document.getElementById("link");
+    var infoButton = document.getElementById("info_button");
+    var infoContainer = document.getElementById("i_c");
+
+    infoButton.addEventListener("mouseout", function () {
+        infoContainer.style.display = "none";
+    });
+    infoButton.addEventListener("mouseover", function () {
+        infoContainer.style.display = "block";
+    });
 
     submitButton.addEventListener("click", submitMessage);
     copyButton.addEventListener("click", copyToClipboard);
 
     function submitMessage() {
         var message = messageInput.value;
+        var salt = saltInput.value;
 
         fetch(document.documentURI, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({ message })
+            body: JSON.stringify({ message, salt})
         })
             .then((response) => {
                 if (!response.ok) {
@@ -28,6 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 linkElement.href = data.message.link;
                 copyButton.hidden = false;
                 messageInput.value = "";
+                saltInput.value = "";
             })
             .catch((error) => {
                 console.error("Error:", error);
