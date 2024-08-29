b6d20a544848b18fb55fc4574631649243e41510
cyber <cyber@axians.com>
2024-08-29 10:33:05 +0200

    Rebranding for company

--- static/script/script.js
+++ static/script/script.js
@@ -1,41 +1,86 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var submitButton = document.getElementById("submit-button");
-    var copyButton = document.getElementById("copy-button");
-    var messageInput = document.getElementById("message-input");
-    var saltInput = document.getElementById("salt-input");
-    var linkElement = document.getElementById("link");
-    var initialButtonText = copyButton.innerHTML;
+    const submitButton = document.getElementById("submit-button");
+    const copyButton = document.getElementById("copy-button");
+    const messageInput = document.getElementById("message-input");
+    const saltInput = document.getElementById("salt-input");
+    const ttlInput = document.getElementById("ttl-input");
+    const linkElement = document.getElementById("link");
+    const techHelpToggler = document.getElementById("tht");
+    const shortMain = document.getElementById("main-short");
+    const shortTech = document.getElementById("tech-short");
+    const helpMain = document.getElementById("main-help");
+    const helpTech = document.getElementById("tech-help");
+    const initialMessageValue = messageInput.value;
+    const initialButtonText = copyButton.innerHTML;
 
-    submitButton.addEventListener("click", submitMessage);
-    copyButton.addEventListener("click", copyToClipboard);
+    techHelpToggler.checked = false
+    helpTech.style.display = "none";
+    shortTech.style.display = "none";
 
-    function submitMessage() {
-        var message = messageInput.value;
-        var salt = saltInput.value;
+    techHelpToggler.addEventListener("change", function() {
+        if(techHelpToggler.checked) {
+            console.log("Display help")
+            shortTech.style.display = "";
+            shortMain.style.display = "none";
+            helpTech.style.display = "";
+            helpMain.style.display = "none";
+        } else {
+            console.log("Dont display help")
+            shortTech.style.display = "none";
+            shortMain.style.display = "";
+            helpTech.style.display = "none";
+            helpMain.style.display = "";
+        }
 
-        fetch(document.documentURI, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ message, salt})
-        })
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error("Network response was not ok");
-                }
-                return response.json();
-            })
-            .then((data) => {
-                linkElement.innerHTML = data.message.link;
-                linkElement.href = data.message.link;
-                copyButton.hidden = false;
-                messageInput.value = "";
-                saltInput.value = "";
-            })
-            .catch((error) => {
-                console.error("Error:", error);
+    });
+
+    submitButton.addEventListener("click", async () => {
+        try {
+            const message = messageInput.value;
+            const salt = saltInput.value;
+            const ttl = ttlInput.value;
+            const newSalt = await generateSalt();
+            console.log(newSalt);
+
+            const response = await fetch(document.documentURI, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ message, salt, ttl })
             });
+
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+
+            const data = await response.json();
+            linkElement.innerHTML = data.message.link;
+            linkElement.href = data.message.link;
+            copyButton.hidden = false;
+            messageInput.value = initialMessageValue;
+            saltInput.value = newSalt;
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    });
+
+    async function generateSalt() {
+        try {
+            const response = await fetch('https://en.wikipedia.org/api/rest_v1/page/random/title');
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch random page');
+            }
+
+            const data = await response.json();
+            const title = data.items[0].title;
+            const salt = title.replace(/[^a-zA-Z0-9]/g, '');
+            return salt;
+        } catch (error) {
+            console.error("Error:", error);
+            throw error;
+        }
     }
 
     function copyToClipboard() {
@@ -50,4 +95,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 console.error("Error:", error);
             });
     }
+
+    copyButton.addEventListener("click", copyToClipboard);
 });
--- views/header.tpl
+++ views/header.tpl
@@ -3,8 +3,7 @@
 <head>
     <title>{{title}}</title>
     <link rel="icon" type="image/png" href="favicon.png"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="/static/style/style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <script src="/static/script/script.js"></script>
 </head>
